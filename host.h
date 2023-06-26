be7ebe41b3dc274854b9698513a3cd31d0aa2f26
brewerj3 <brewerj3@hawaii.edu>
2023-06-26 11:41:57 -1000

    added host.h and host.c

---
+++ host.h
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  Ballistics04 - Spr 2023
+///
+/// @file host.h
+/// @version 1.0
+///
+/// @author Joshua Brewer <brewerj3@hawaii.edu>
+/// @date   26_Jun_2023
+///////////////////////////////////////////////////////////////////////////////
+#ifndef BALLISTICS04_HOST_H
+#define BALLISTICS04_HOST_H
+
+#endif //BALLISTICS04_HOST_H
