657d01d4d02e8c73a3dd6dbedfc6d7f26883137d
brewerj3 <brewerj3@hawaii.edu>
2023-06-26 10:02:12 -1000

    worked on main.c

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
 
 int main() {
-    printf("Hello, World!\n");
+    pid_t pid;
+
+    // @TODO make the pipes for the manager and host here
+
+
+
+    pid = fork();
+
+    if(pid == -1) {
+        printf("Error: the fork() failed\n");
+        return 0;
+    } else if(pid == 0) { // The child process
+        host_main();    // @TODO Implement the host routine
+        return 0;
+    }
+
+    // Parent process executes manager routine
+    man_main();
+
+    // If user quits manager routine, kill all child processes
+    kill(0, SIGKILL);   // @TODO Implement the manager routine
     return 0;
 }
